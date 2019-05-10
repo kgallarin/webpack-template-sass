2050cb27315e1627f84902387752b8b85fb68d8b
gallarinkevinm@gmail.com <gallarinkevinm@gmail.com>
2019-05-11 03:01:23 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/js/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["es2015"] }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
+      test: /\.(png|jpe?g|gif|ico)$/,
+      use: [
+            {
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'img/',
+                    publicPath: 'dist/img/'
+                }
+            }
+        ]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        exclude: /node_modules/,
+        loader: 'file-loader?limit=1024&name=fonts/[name].[ext]'
+      },
+      { test: /\.(ttf|eot|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        exclude: /node_modules/,
+        loader: "file-loader?limit=1024&name=fonts/[name].[ext]"
+      }
+    ]
+  }
+};
